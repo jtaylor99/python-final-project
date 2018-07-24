fa062c5700f2b7d98b3951e574d72360158e7b88
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-24 13:01:49 -0500

    added more to the shell

--- shell.py
+++ shell.py
@@ -4,7 +4,21 @@ import disk
 
 def main():
     print('Welcome to the Rental store!')
-    user_response = input('How can I help you today?')
+    user_response = input('Would you like to rent anything?')
+    # this is a yes or no question
+    if user_response == 'yes':
+        print('These is what we have in stock')
+        print(inventory)
+    elif user_response == 'no':
+        print('Thank you have a great day!')
+        exit()
+    else:
+        print('Please choose a valid option!')
+
+    selection = input('Which one would you would like to rent?')
+    for selection in inventory:
+        if selection in inventory:
+            print(selection)
 
 
 if __name__ == '__main__':
