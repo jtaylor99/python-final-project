f4bc329b294b7fd825c4f6f8eb576fb713477e7b
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-24 09:49:12 -0500

    starting in the shell

---
+++ shell.py
@@ -0,0 +1,11 @@
+import core
+import disk
+
+
+def main():
+    print('Welcome to the Rental store!')
+    user_response = input('How can I help you today?')
+
+
+if __name__ == '__main__':
+    main()
