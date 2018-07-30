f25b30caab25cc51ebd5a9c5279705278c60c9d5
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-07-30 15:36:56 -0500

    I fix my problem from Friday and now I'm trying to get the program running

--- core.py
+++ core.py
@@ -1,31 +1,18 @@
 def create_rent_dictionary(rent_list):
     rent_dictionary = {}
     for rent_info in rent_list:
-        items = rent_info.split(',')
-        name = items[0].split(',')
-        stock = int(items[1])
-        price = int(items[2])
-        replacement_cost = int(items[3])
-        rent_dictionary = name, stock, price, replacement_cost
+        item = rent_info.split(',')
+        key = item[0].strip()
+        value_1 = int(item[1].strip())
+        value_2 = int(item[2].strip())
+        value_3 = int(item[3].strip())
+        rent_dictionary[key] = value_1, value_2, value_3
     return rent_dictionary
 
 
-def rental_rates(rent_dictionary, item_name):
-    '''(int,int) -> int
-    Returns the rental rates 
-    '''
-    sales_tax = 0.07 + 1
-    price = rent_dictionary[item_name][1] * sales_tax
-    replacement_cost = rent_dictionary[item_name][2] * 0.10
-    rental_rates = price + replacement_cost
-    return rental_rates
-
-
 def create_file_string(rent_dictionary):
-    ''' ({str: int} -> str)
-    turns the user_dictionary
-    '''
-    file_string = 'name', stock, price, replacement_cost
-    for name in rent_dictionary.items():
-        file_string = f'{name} ({stock}, {int(price)}, {int(replacement_cost)})'
+    file_string = 'name, stock, price, replacement_cost'
+    for key, value in rent_dictionary.items():
+        name, stock, price, replacement_cost = key, value_1, value_2, value_3
+        file_string = f'{name} ({int(stock)}, {int(price)}, {int(replacement_cost)})'
     return file_string
