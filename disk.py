890eb347b040f23f1a91c47b6e83d81546b5b788
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-08-03 13:27:11 -0500

    I fixed the disk

--- disk.py
+++ disk.py
@@ -9,6 +9,12 @@ def open_file():
     return file_info
 
 
+def write_the_history(user_name, selection):
+    text = f'{user_name.upper()} checked out {selection}\n'
+    with open('history.txt', 'a') as file:
+        file.write(text)
+
+
 def write_file(file_string):
-    with open(inventory.txt, 'a') as file:
+    with open('inventory.txt', 'w') as file:
         file.write(file_string)
