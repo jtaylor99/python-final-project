cc792f48fb7976889b696e37364961016991fe26
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-08-07 11:08:20 -0500

    I'm finished with the shell

--- shell.py
+++ shell.py
@@ -2,37 +2,29 @@ import core
 import disk
 
 
-def rental_rates(rent_dictionary, item_name):
-    '''(int,int) -> int
-    Returns the rental rates 
-    '''
-    sales_tax = 0.07 + 1
-    price = rent_dictionary[item_name][1] * sales_tax
-    replacement_cost = rent_dictionary[item_name][2] * 0.10
-    rental_rates = price + replacement_cost
-    return rental_rates
-
-
 def main():
     filename = 'inventory.txt'
     rent_info = disk.open_file()
-    rent_dictionary = core.create_rent_dictionary(rent_info)
+    rent_dictionary = core.create_rent_dictionary()
     print('Welcome to the Rental store!')
     name = input('What\'s your name?:')
     print(f'Hello {name.upper()}')
-    user_response = input('Would you like to rent anything?')
-    # this is a yes or no question
-    if user_response == 'yes':
-        print('These is what we have in stock')
-        print(rent_dictionary)
-    elif user_response == 'no':
-        returning = input('What are you returning?:')
-        if returning in rent_dictionary:
-            print(f'Thank you for returning {returning} and have a great day')
-            exit()
-        else:
+    while True:
+        user_response = input('Would you like to rent anything?')
+        # this is a yes or no question
+        if user_response == 'yes':
+            print('These is what we have in stock')
             print(rent_dictionary)
-            print('Please choose a valid option!')
+            break
+        elif user_response == 'no':
+            returning = input('What are you returning?:')
+            if returning in rent_dictionary:
+                print(
+                    f'Thank you for returning {returning} and have a great day'
+                )
+                exit()
+            else:
+                print('Please choose a valid option!')
     while True:
         selection = input('Which one would you would like to rent?')
         if selection in rent_dictionary:
@@ -40,7 +32,7 @@ def main():
             break
         if selection not in rent_dictionary:
             print('please choose a valid option!')
-    total = rental_rates(rent_dictionary, selection)
+    total = core.rental_rates(rent_dictionary, selection)
     days = input('How many days do you want to rent this?')
     if days.isdigit():
         total = total * int(days)
