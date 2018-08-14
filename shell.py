587bd80b75951e232efe2bc9022666346ac2194c
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-08-14 15:37:10 -0500

    I added a function that take 1 out of the stock

--- shell.py
+++ shell.py
@@ -5,12 +5,12 @@ import disk
 def main():
     filename = 'inventory.txt'
     rent_info = disk.open_file()
-    rent_dictionary = core.create_rent_dictionary()
+    rent_dictionary = core.create_rent_dictionary(rent_info)
     print('Welcome to the Rental store!')
-    name = input('What\'s your name?:')
+    name = input('What\'s your name?: ')
     print(f'Hello {name.upper()}')
     while True:
-        user_response = input('Would you like to rent anything?')
+        user_response = input('Would you like to rent anything? ')
         # this is a yes or no question
         if user_response == 'yes':
             print('These is what we have in stock')
@@ -18,7 +18,7 @@ def main():
             break
         elif user_response == 'no':
             print(rent_dictionary)
-            returning = input('What are you returning?:')
+            returning = input('What are you returning?: ')
             if returning in rent_dictionary:
                 print(
                     f'Thank you for returning {returning} and have a great day'
@@ -27,14 +27,14 @@ def main():
             else:
                 print('Please choose a valid option!')
     while True:
-        selection = input('Which one would you would like to rent?:')
+        selection = input('Which one would you would like to rent?: ')
         if selection in rent_dictionary:
             print(f'you have selected {selection}')
             break
         if selection not in rent_dictionary:
             print('please choose a valid option!')
     total = core.rental_rates(rent_dictionary, selection)
-    days = input('How many days do you want to rent this?')
+    days = input('How many days do you want to rent this?: ')
     if days.isdigit():
         total = total * int(days)
     print('----------------')
@@ -43,6 +43,7 @@ def main():
     print(f'total: {total:.2f}')
     print('Thank for your service')
     print('----------------')
+    core.stock(selection)
     disk.write_the_history(name, selection)
     file_string = core.create_file_string(rent_dictionary)
     disk.write_file(file_string)
