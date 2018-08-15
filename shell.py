02bbb9c16a5064b8f014971354cf40ccf52f7575
jtaylor99 <justicetaylor@basecampcodingacademy.org>
2018-08-15 14:44:34 -0500

    I dix the hard code

--- shell.py
+++ shell.py
@@ -2,10 +2,17 @@ import core
 import disk
 
 
+def print_dictionary(rent_dictionary):
+    for key in rent_dictionary:
+        print('{}, stock:{}, price:{}, replacement cost:{}'.format(
+            key, rent_dictionary[key]['stock'], rent_dictionary[key]['price'],
+            rent_dictionary[key]['replacement cost']))
+
+
 def main():
-    filename = 'inventory.txt'
-    rent_info = disk.open_file()
+    rent_info = disk.open_file('inventory.txt')
     rent_dictionary = core.create_rent_dictionary(rent_info)
+
     print('Welcome to the Rental store!')
     name = input('What\'s your name?: ')
     print(f'Hello {name.upper()}')
@@ -14,22 +21,25 @@ def main():
         # this is a yes or no question
         if user_response == 'yes':
             print('These is what we have in stock')
-            print(rent_dictionary)
+            print_dictionary(rent_dictionary)
             break
         elif user_response == 'no':
             print(rent_dictionary)
             returning = input('What are you returning?: ')
-            if returning in rent_dictionary:
-                print(
-                    f'Thank you for returning {returning} and have a great day'
-                )
-                exit()
-            else:
-                print('Please choose a valid option!')
+            print(f'Thank you for returning {returning} and have a great day')
+            core.return_item(rent_dictionary, returning)
+            file_string = core.create_file_string(rent_dictionary)
+            disk.write_file(file_string, 'inventory.txt')
+            exit()
+
     while True:
         selection = input('Which one would you would like to rent?: ')
         if selection in rent_dictionary:
-            print(f'you have selected {selection}')
+            if rent_dictionary[selection]['stock'] == 0:
+                print('It\'s already rented')
+                continue
+            else:
+                print(f'you have selected {selection}')
             break
         if selection not in rent_dictionary:
             print('please choose a valid option!')
@@ -43,10 +53,10 @@ def main():
     print(f'total: {total:.2f}')
     print('Thank for your service')
     print('----------------')
-    core.stock(selection)
-    disk.write_the_history(name, selection)
+    core.reduce_stock(rent_dictionary, selection)
+    disk.write_the_history(name, selection, 'history.txt')
     file_string = core.create_file_string(rent_dictionary)
-    disk.write_file(file_string)
+    disk.write_file(file_string, 'inventory.txt')
 
 
 if __name__ == '__main__':
